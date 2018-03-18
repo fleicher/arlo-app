43d4f144106df6d83cdb873d1e0a19690f222fc2
florian <leicher@fzi.de>
2018-03-18 19:38:07 +0100

    removed most unnecessary modules

--- messaging/app/src/main/java/com/google/firebase/quickstart/fcm/MainActivity.java
+++ messaging/app/src/main/java/com/google/firebase/quickstart/fcm/MainActivity.java
@@ -19,13 +19,31 @@ package com.google.firebase.quickstart.fcm;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.NetworkOnMainThreadException;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.net.MalformedURLException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
@@ -33,12 +51,13 @@ import com.google.firebase.messaging.FirebaseMessaging;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    HashMap<String, String> sentValues = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        System.out.println("starting activity");
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             // Create channel to show notifications.
             String channelId  = getString(R.string.default_notification_channel_id);
@@ -58,15 +77,66 @@ public class MainActivity extends AppCompatActivity {
         //
         // Handle possible data accompanying notification message.
         // [START handle_data_extras]
+        /*
         if (getIntent().getExtras() != null) {
+            System.out.println("Extras not null");
             for (String key : getIntent().getExtras().keySet()) {
-                Object value = getIntent().getExtras().get(key);
+                String value = (String) getIntent().getExtras().get(key);
                 Log.d(TAG, "Key: " + key + " Value: " + value);
+                sentValues.put(key, value);
             }
         }
+        else{
+            System.out.println("Extras null");
+        }
         // [END handle_data_extras]
+        sentValues.put("image", "http://res.cloudinary.com/arlo/image/upload/v1521292110/last.png");
+        */
 
-        Button subscribeButton = findViewById(R.id.subscribeButton);
+        final SharedPreferences sharedPrefs = getSharedPreferences("my_prefs", 0);
+        final Button videoURLButton = findViewById(R.id.videoButton);
+
+        //videoURLButton.setText("Platzhalter");
+        final TextView tw = findViewById(R.id.informationTextView);
+
+        if (sharedPrefs.getLong("stamp", 0L) + 1000000L > System.currentTimeMillis()) {
+            new DownloadInfo().execute();
+
+            String info_text = sharedPrefs.getString("name", "No Camera Name") + " " +
+                    sharedPrefs.getString("date", "No Recording Date") + " @ " +
+                    sharedPrefs.getString("time", "-");
+            tw.setText(info_text);
+            System.out.println("j" + sharedPrefs.getString("name", "nothing"));
+
+
+            //Button videoURLButton = findViewById(R.id.videoButton);
+            videoURLButton.setText("Download Video");
+            videoURLButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String url = sharedPrefs.getString("url", "https://google.com");
+                    Uri uri = Uri.parse(url); // missing 'http://' will cause crashed
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+
+                    // Get token
+                    /*
+                    String token = FirebaseInstanceId.getInstance().getToken();
+
+                    // Log and toast
+                    String msg = "02 " + getString(R.string.msg_token_fmt, token);
+                    Log.d(TAG, msg);
+                    Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    */
+                }
+            });
+
+        }else{
+            //Button videoURLButton = findViewById(R.id.videoButton);
+            //videoURLButton.setText("Video Expired");
+        }
+
+        final Button subscribeButton = findViewById(R.id.subscribeButton);
         subscribeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -81,19 +151,59 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button logTokenButton = findViewById(R.id.logTokenButton);
-        logTokenButton.setOnClickListener(new View.OnClickListener() {
+        Button falseAlarmButton = findViewById(R.id.deleteButton);
+        falseAlarmButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                // Get token
-                String token = FirebaseInstanceId.getInstance().getToken();
-
-                // Log and toast
-                String msg = getString(R.string.msg_token_fmt, token);
-                Log.d(TAG, msg);
-                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            public void onClick(View v){
+            sharedPrefs.edit().clear().apply();
+            videoURLButton.setOnClickListener(null);
+            videoURLButton.setText("No Video");
+            tw.setText("marked as false Alarm");
             }
         });
     }
+    private class DownloadInfo extends AsyncTask<Void, Void, Bitmap>{
 
+        @Override
+        protected Bitmap doInBackground(Void... params){
+            SharedPreferences sharedPrefs = getSharedPreferences("my_prefs", 0);
+            System.out.println("called in Bg");
+            if (sharedPrefs.getLong("stamp", 0L)
+                    + 1000000L>System.currentTimeMillis() ) {
+                try {
+                    System.out.println("Starting image download " +
+                            sharedPrefs.getString("image", null));
+                    URL u = new URL(sharedPrefs.getString("image", null));
+                    InputStream inp = u.openStream();
+                    Bitmap bitmap = BitmapFactory.decodeStream(inp);
+                    System.out.println("Stopping image download");
+                    return bitmap;
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (NetworkOnMainThreadException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    Writer writer = new StringWriter();
+                    e.printStackTrace(new PrintWriter(writer));
+                    String s = writer.toString();
+                    e.printStackTrace();
+                    System.out.println("Error: " + s);
+                }
+            }
+            else{
+                System.out.println("last occurence too old");
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result){
+            ImageView i = findViewById(R.id.camera);
+            i.setImageBitmap(result);
+        }
+
+    }
 }
+
--- messaging/app/src/main/java/com/google/firebase/quickstart/fcm/MyFirebaseMessagingService.java
+++ messaging/app/src/main/java/com/google/firebase/quickstart/fcm/MyFirebaseMessagingService.java
@@ -21,9 +21,13 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+
+import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.Settings;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -33,6 +37,8 @@ import com.firebase.jobdispatcher.GooglePlayDriver;
 import com.firebase.jobdispatcher.Job;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import java.util.Map;
+import java.sql.Timestamp;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
@@ -56,6 +62,13 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
         // [END_EXCLUDE]
 
+        /*
+        Display Messages: These messages trigger the onMessageReceived() callback only when your app is in foreground
+            set the "notification" key (and optionally the "data" key)
+        Data Messages: Theses messages trigger the onMessageReceived() callback even if your app is in foreground/background/killed
+            only set the "data" key and not the "notification" key
+        */
+
         // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
@@ -63,20 +76,25 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+            System.out.println("hey");
 
-            if (/* Check if data needs to be processed by long running job */ true) {
+            if (/* Check if data needs to be processed by long running job */ false) {
                 // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
                 scheduleJob();
+                System.out.println("true");
             } else {
                 // Handle message within 10 seconds
-                handleNow();
+                System.out.println("false");
+                handleNow(remoteMessage.getData());
             }
 
         }
 
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
+            System.out.println("body");
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            //handleNow(remoteMessage.getData());
         }
 
         // Also if you intend on generating your own notifications as a result of a received FCM
@@ -101,8 +119,20 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     /**
      * Handle time allotted to BroadcastReceivers.
      */
-    private void handleNow() {
+    private void handleNow(Map<String, String> data) {
+        System.out.println("now");
+        SharedPreferences sharedPrefs = getSharedPreferences("my_prefs", 0);
+        SharedPreferences.Editor editor= sharedPrefs.edit();
+        String[] keys = {"name", "date", "url", "image", "time"};
+        for (String key : keys){
+            editor.putString(key, data.get(key));
+        }
+        editor.putLong("stamp", System.currentTimeMillis());
+        editor.apply();
+
         Log.d(TAG, "Short lived task is done.");
+        sendNotification(data.get("name"));
+        System.out.println("h" + sharedPrefs.getString("name", "nothing"));
     }
 
     /**
@@ -111,6 +141,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      * @param messageBody FCM message body received.
      */
     private void sendNotification(String messageBody) {
+        Log.d(TAG, "call to sendNotification");
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
@@ -118,14 +149,20 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         String channelId = getString(R.string.default_notification_channel_id);
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Uri customSoundUri = Uri.parse("android.resource://com.google.firebase.quickstart.fcm/" + R.raw.katchi);
+        long[] pattern = {0,100,200,300,400,500,500,500,500};
+
+
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(this, channelId)
                 .setSmallIcon(R.drawable.ic_stat_ic_notification)
-                .setContentTitle("FCM Message")
+                .setContentTitle("Suspicious Movement Detected")
                 .setContentText(messageBody)
                 .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent);
+                .setSound(customSoundUri)
+                .setContentIntent(pendingIntent)
+                .setLights(Color.BLUE, 500, 500)
+                .setVibrate(pattern);
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
